4162cbf42529a6d319ff5f9388df91ff74893b54
tzzzich <bvv005tkun@gmail.com>
2024-06-14 11:53:03 +0700

    feat(components): add header, add login page

---
+++ src/pages/Login.jsx
@@ -0,0 +1,21 @@
+import './login.css'
+
+export default function LoginPage () {
+
+    return (
+        <>
+            <div className="row">
+                <div className="col">
+                    <div className="sponsor-info">
+                        
+                    </div>
+                </div>
+                <div className="col">
+                    <div className="login-form">
+                        <input></input>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+};
---
+++ src/utils/constants/router.js
@@ -0,0 +1,6 @@
+export const ROUTES = {
+    ROOT: '/',
+    LOGIN: '/login',
+    REGISTRATION: '/registration'
+  };
+  
