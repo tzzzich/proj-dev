b8878e054eae20b66f10ee2ed6211c64c7fe0748
tzzzich <bvv005tkun@gmail.com>
2024-06-15 15:06:41 +0700

    feat(pages): add login page
    Add login page featuring login form.

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,21 +1,28 @@
+import { Link } from 'react-router-dom'
 import './login.css'
 
 export default function LoginPage () {
 
     return (
-        <>
-            <div className="row">
-                <div className="col">
-                    <div className="sponsor-info">
-                        
-                    </div>
-                </div>
-                <div className="col">
+        <div className="content-wrapper">
+
+            <div className="sponsor-info">
+
+            </div>
+
+            <div className="input-form-wrapper">
+                <div className="login-form-wrapper">
                     <div className="login-form">
-                        <input></input>
+                        <h2>Hello Again!</h2>
+                        <input type="email" placeholder="Email" />
+                        <input type="email" placeholder="Email" />
+                        <button type="submit">Submit</button>
+                        <p className="text-sm">Don't have an account yet?</p>
+                        <h3>Register now</h3>
                     </div>
                 </div>
             </div>
-        </>
+
+        </div>
     );
 };
